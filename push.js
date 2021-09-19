9308c6b6d1c501bf05a3615efcbcab8e0926ba1f
Pilar Garcia <pgarcia4@correo.um.edu.uy>
2021-09-19 20:28:40 -0300

    a ver

--- push.js
+++ push.js
@@ -5,7 +5,7 @@ let vapidKeys = {
     privateKey: 'xrKPFOLRHGe3lnOu43AA1FUzd6ueX9MVmrSJJKCwLBQ'
 }
 
-push.setVapidDetails('mailto: test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
+push.setVapidDetails('mailto: pgarcia4@correo.um.edu.uy', vapidKeys.publicKey, vapidKeys.privateKey)
 
 let sub = {"endpoint":"https://updates.push.services.mozilla.com/wpush/v1/gAAAAABhR…X1qqlQLYiNYVLKylod6TW4BVwVxddx-cw73YXgkjb9CeiFW0geIiLAnDVlnZ","keys":{"auth":"O5HoYRSVRqmosJon2GpJ5A","p256dh":"BKp4qEvMvmXP-FVlUbgYaLO5DY9BpJSqwUxVRKfyOe2RCe1pOW9M3tjoIlOiU-2H7asEU1C09XzDMw7yxFGWniw"}};
 push.sendNotification(sub, 'test message')
